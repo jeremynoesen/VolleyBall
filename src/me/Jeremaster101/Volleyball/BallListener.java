273943a695554ff729f14e82e2a41b39d4b0aefc
Jeremy Noesen <46114593+jeremaster101@users.noreply.github.com>
2019-07-19 18:15:03 -0500

    Separated classes, added custom messages

---
+++ src/me/Jeremaster101/Volleyball/BallListener.java
@@ -0,0 +1,84 @@
+package me.Jeremaster101.Volleyball;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityTargetEvent;
+import org.bukkit.event.player.PlayerInteractAtEntityEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.util.Vector;
+
+public class BallListener implements Listener {
+
+    /**
+     * test when a player hits the volleyball so the ball can be launched in that direction
+     */
+    @EventHandler
+    public void onBallHit(EntityDamageByEntityEvent e) {
+        if (e.getDamager() instanceof Player && e.getEntity().getCustomName() != null && e.getEntity()
+                .getCustomName().equals(ChatColor.DARK_GREEN +
+
+                        "" + ChatColor.BOLD + "BALL")) {
+            e.setCancelled(true);
+            Entity s = e.getEntity();
+            s.getWorld().playSound(s.getLocation(), Sound.ENTITY_CHICKEN_EGG, 2, 0);
+            s.setVelocity(e.getDamager().getLocation().getDirection().multiply(0.9).add(new Vector(0, 0.6, 0))
+                    .add(e.getDamager().getVelocity().setY(e.getDamager().getVelocity().multiply(2).getY())));
+        }
+
+        if (e.getDamager() instanceof Player && e.getEntity().getCustomName() != null && e.getEntity().getCustomName().equals("BALLSTAND")) {
+            for (Entity s : e.getEntity().getNearbyEntities(1, 1, 1)) {
+                if (s.getCustomName() != null && s.getCustomName().equals(ChatColor.DARK_GREEN +
+                        "" + ChatColor.BOLD + "BALL")) {
+                    s.getWorld().playSound(s.getLocation(), Sound.ENTITY_CHICKEN_EGG, 2, 0);
+                    s.setVelocity(e.getDamager().getLocation().getDirection().multiply(0.9).add(new Vector(0, 0.6, 0))
+                            .add(e.getDamager().getVelocity().setY(e.getDamager().getVelocity().multiply(2).getY())));
+                    break;
+                }
+            }
+        }
+    }
+
+    /**
+     * protect the court from breaking
+     */
+    @EventHandler
+    public void onInteract(PlayerInteractEvent e) {
+        Player p = e.getPlayer();
+        Action a = e.getAction();
+        if (a == Action.LEFT_CLICK_AIR || a == Action.LEFT_CLICK_BLOCK) {
+            if (p.getLocation().getBlock().getRelative(0, -2, 0).getType().equals(Material.LAPIS_BLOCK) ||
+                    p.getLocation().getBlock().getRelative(0, -3, 0).getType().equals(Material.LAPIS_BLOCK))
+                e.setCancelled(true);
+        }
+    }
+
+    /**
+     * block the slime from targetting players in survival mode
+     */
+
+    @EventHandler
+    public void onEntityTarget(EntityTargetEvent e) {
+        Entity s = e.getEntity();
+        if (s.getCustomName() != null && s.getCustomName().equals(ChatColor.DARK_GREEN +
+                "" + ChatColor.BOLD + "BALL") && e.getTarget() instanceof Player) e.setCancelled(true);
+    }
+
+    /**
+     * prevent players from taking the head off the volleyball armorstand
+     */
+    @EventHandler
+    public void onEntityInteract(PlayerInteractAtEntityEvent e) {
+        Player p = e.getPlayer();
+        if (e.getRightClicked() != null && e.getRightClicked() instanceof ArmorStand && e.getRightClicked().getCustomName()
+                != null && e.getRightClicked().getCustomName().equals("BALLSTAND"))
+            e.setCancelled(true);
+    }
+}
---
+++ src/me/Jeremaster101/Volleyball/Message.java
@@ -0,0 +1,116 @@
+package me.Jeremaster101.Volleyball;
+
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+import java.util.logging.Level;
+
+/**
+ * All messages used within the plugin
+ */
+public class Message {
+    private static File messageConfigFile = null;
+    private static YamlConfiguration config = null;
+
+    static String PREFIX = format(getConfig().getString("PREFIX"));
+    static String ERROR_NOT_ON_COURT = PREFIX + format(getConfig().getString("ERROR_NOT_ON_COURT"));
+    static String ERROR_BALL_OUT = PREFIX + format(getConfig().getString("ERROR_BALL_OUT"));
+    public String STARTUP = "\n\n" +
+            ChatColor.DARK_GRAY + "███╗" + ChatColor.YELLOW + "██╗   ██╗" + ChatColor.GOLD + "██████╗ " + ChatColor.DARK_GRAY + "███╗" + ChatColor.WHITE + "  Volleyball version " + Volleyball.plugin.getDescription().getVersion() + " " + "has " + "been enabled!\n" +
+            ChatColor.DARK_GRAY + "██╔╝" + ChatColor.YELLOW + "██║   ██║" + ChatColor.GOLD + "██╔══██╗" + ChatColor.DARK_GRAY + "╚██║" + ChatColor.WHITE + "  Volleyball is written by Jeremaster101 and\n" +
+            ChatColor.DARK_GRAY + "██║ " + ChatColor.YELLOW + "██║   ██║" + ChatColor.GOLD + "██████╔╝" + ChatColor.DARK_GRAY + " ██║" + ChatColor.WHITE + "  may not be modified or redistributed without\n" +
+            ChatColor.DARK_GRAY + "██║ " + ChatColor.YELLOW + "╚██╗ ██╔╝" + ChatColor.GOLD + "██╔══██╗" + ChatColor.DARK_GRAY + " ██║" + ChatColor.WHITE + "  his consent. For help and support, join the\n" +
+            ChatColor.DARK_GRAY + "███╗" + ChatColor.YELLOW + " ╚████╔╝ " + ChatColor.GOLD + "██████╔╝" + ChatColor.DARK_GRAY + "███║" + ChatColor.WHITE + "  support discord group: https://discord.gg/WhmQYR\n" +
+            ChatColor.DARK_GRAY + "╚══╝" + ChatColor.YELLOW + "  ╚═══╝  " + ChatColor.GOLD + "╚═════╝ " + ChatColor.DARK_GRAY + "╚══╝" + ChatColor.WHITE + "  Thank you for choosing Volleyball!\n";
+    String CLEANING = PREFIX + ChatColor.GRAY + "Deleting leftover volleyball entities...";
+    String DONE_CLEANING =
+            PREFIX + ChatColor.GRAY + "Successfully deleted " + ChatColor.WHITE + "$COUNT$" + ChatColor.GRAY +
+                    " volleyball entities!";
+    String[] HELP = new String[]{
+            "",
+            format("\n&8&l---------[&e&lVollay&6&lball &7&lHelp&8&l]---------"),
+            ChatColor.GRAY + "/volleyball | /vb <message>" + ChatColor.WHITE + ": Serve a volleyball",
+            ChatColor.DARK_GRAY + "" + ChatColor.BOLD + "----------------------------------",
+            ""
+    };
+
+    /**
+     * Apply color codes and line breaks to a message
+     *
+     * @param msg message to format with color codes and line breaks
+     * @return formatted message
+     */
+    public static String format(String msg) {
+        return ChatColor.translateAlternateColorCodes('&', msg);
+    }
+
+    /**
+     * Reloads the message file and reassigns the messages to the updated values
+     */
+    public static void reloadConfig() {
+        if (messageConfigFile == null) {
+            messageConfigFile = new File(Volleyball.plugin.getDataFolder(), "messages.yml");
+            config = YamlConfiguration.loadConfiguration(messageConfigFile);
+        }
+        try {
+            config.load(messageConfigFile);
+        } catch (IOException | InvalidConfigurationException ignored) {
+        }
+
+        Reader defConfigStream = new InputStreamReader(Volleyball.plugin.getResource("messages.yml"),
+                StandardCharsets.UTF_8);
+        YamlConfiguration defConfig = YamlConfiguration.loadConfiguration(defConfigStream);
+        config.setDefaults(defConfig);
+        config.options().copyDefaults(true);
+        saveConfig();
+
+        PREFIX = format(config.getString("PREFIX"));
+        ERROR_BALL_OUT = PREFIX + format(config.getString("ERROR_BALL_OUT"));
+        ERROR_NOT_ON_COURT = PREFIX + format(config.getString("ERROR_SENT_BEFORE"));
+    }
+
+    /**
+     * Getter for the message config file
+     *
+     * @return message config
+     */
+    public static YamlConfiguration getConfig() {
+        if (config == null) {
+            reloadConfig();
+        }
+        return config;
+    }
+
+    /**
+     * Saves the message config file
+     */
+    public static void saveConfig() {
+        if (config == null || messageConfigFile == null) {
+            return;
+        }
+        try {
+            getConfig().save(messageConfigFile);
+        } catch (IOException ex) {
+            Volleyball.plugin.getLogger().log(Level.SEVERE, "Could not save config to " + messageConfigFile, ex);
+        }
+    }
+
+    /**
+     * Saves default message config when the server loads
+     */
+    public static void saveDefaultConfig() {
+        if (messageConfigFile == null) {
+            messageConfigFile = new File(Volleyball.plugin.getDataFolder(), "messages.yml");
+        }
+        if (!messageConfigFile.exists()) {
+            Volleyball.plugin.saveResource("messages.yml", false);
+        }
+    }
+
+}
