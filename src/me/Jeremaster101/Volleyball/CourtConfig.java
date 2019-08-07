9e4e360bb68bb188e769e0c16c5e84731750e483
Jeremy Noesen <46114593+jeremaster101@users.noreply.github.com>
2019-08-06 19:34:33 -0500

    Separated MessageConfig from Message class, started work on region based courts

---
+++ src/me/Jeremaster101/Volleyball/CourtConfig.java
@@ -0,0 +1,75 @@
+package me.Jeremaster101.Volleyball;
+
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+import java.util.logging.Level;
+
+/**
+ * Class used to manage the court config file
+ */
+public class CourtConfig {
+    private static File courtConfigFile = null;
+    private static YamlConfiguration config = null;
+    
+    /**
+     * Reloads the court file
+     */
+    public static void reloadConfig() {
+        if (courtConfigFile == null) {
+            courtConfigFile = new File(Volleyball.plugin.getDataFolder(), "courts.yml");
+        }
+        
+        config = YamlConfiguration.loadConfiguration(courtConfigFile);
+        
+        Reader defConfigStream = new InputStreamReader(Volleyball.plugin.getResource("courts.yml"),
+                StandardCharsets.UTF_8);
+        YamlConfiguration defConfig = YamlConfiguration.loadConfiguration(defConfigStream);
+        config.setDefaults(defConfig);
+        config.options().copyDefaults(true);
+        saveConfig();
+    }
+    
+    /**
+     * Getter for the court config file
+     *
+     * @return court config
+     */
+    public static YamlConfiguration getConfig() {
+        if (config == null) {
+            reloadConfig();
+        }
+        return config;
+    }
+    
+    /**
+     * Saves the court config file
+     */
+    public static void saveConfig() {
+        if (config == null || courtConfigFile == null) {
+            return;
+        }
+        try {
+            getConfig().save(courtConfigFile);
+        } catch (IOException ex) {
+            Volleyball.plugin.getLogger().log(Level.SEVERE, "Could not save config to " + courtConfigFile, ex);
+        }
+    }
+    
+    /**
+     * Saves default court config when the server loads
+     */
+    public static void saveDefaultConfig() {
+        if (courtConfigFile == null) {
+            courtConfigFile = new File(Volleyball.plugin.getDataFolder(), "courts.yml");
+        }
+        if (!courtConfigFile.exists()) {
+            Volleyball.plugin.saveResource("courts.yml", false);
+            config = YamlConfiguration.loadConfiguration(courtConfigFile);
+        }
+    }
+}
--- src/me/Jeremaster101/Volleyball/Message.java
+++ src/me/Jeremaster101/Volleyball/Message.java
@@ -1,25 +1,15 @@
 package me.Jeremaster101.Volleyball;
 
 import org.bukkit.ChatColor;
-import org.bukkit.configuration.file.YamlConfiguration;
-
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.nio.charset.StandardCharsets;
-import java.util.logging.Level;
 
 /**
  * All messages used within the plugin
  */
 public class Message {
-    private static File messageConfigFile = null;
-    private static YamlConfiguration config = null;
-
-    static String PREFIX = format(getConfig().getString("PREFIX"));
-    static String ERROR_NOT_ON_COURT = PREFIX + format(getConfig().getString("ERROR_NOT_ON_COURT"));
-    static String ERROR_BALL_OUT = PREFIX + format(getConfig().getString("ERROR_BALL_OUT"));
+    static String PREFIX = format(MessageConfig.getConfig().getString("PREFIX"));
+    
+    static String ERROR_BALL_OUT = PREFIX + format(MessageConfig.getConfig().getString("ERROR_BALL_OUT"));
+    static String ERROR_NOT_ON_COURT = PREFIX + format(MessageConfig.getConfig().getString("ERROR_NOT_ON_COURT"));
     public String STARTUP = "\n\n" +
             ChatColor.DARK_GRAY + "███╗" + ChatColor.YELLOW + "██╗   ██╗" + ChatColor.GOLD + "██████╗ " + ChatColor.DARK_GRAY + "███╗" + ChatColor.WHITE + "  Volleyball version " + Volleyball.plugin.getDescription().getVersion() + " " + "has " + "been enabled!\n" +
             ChatColor.DARK_GRAY + "██╔╝" + ChatColor.YELLOW + "██║   ██║" + ChatColor.GOLD + "██╔══██╗" + ChatColor.DARK_GRAY + "╚██║" + ChatColor.WHITE + "  Volleyball is written by Jeremaster101 and\n" +
@@ -38,7 +28,7 @@ public class Message {
             ChatColor.DARK_GRAY + "" + ChatColor.BOLD + "----------------------------------",
             ""
     };
-
+    
     /**
      * Apply color codes and line breaks to a message
      *
@@ -48,66 +38,5 @@ public class Message {
     public static String format(String msg) {
         return ChatColor.translateAlternateColorCodes('&', msg);
     }
-
-    /**
-     * Reloads the message file and reassigns the messages to the updated values
-     */
-    public static void reloadConfig() {
-        if (messageConfigFile == null) {
-            messageConfigFile = new File(Volleyball.plugin.getDataFolder(), "messages.yml");
-        }
-
-        config = YamlConfiguration.loadConfiguration(messageConfigFile);
-
-        Reader defConfigStream = new InputStreamReader(Volleyball.plugin.getResource("messages.yml"),
-                StandardCharsets.UTF_8);
-        YamlConfiguration defConfig = YamlConfiguration.loadConfiguration(defConfigStream);
-        config.setDefaults(defConfig);
-        config.options().copyDefaults(true);
-        saveConfig();
-
-        PREFIX = format(config.getString("PREFIX"));
-        ERROR_BALL_OUT = PREFIX + format(config.getString("ERROR_BALL_OUT"));
-        ERROR_NOT_ON_COURT = PREFIX + format(config.getString("ERROR_NOT_ON_COURT"));
-    }
-
-    /**
-     * Getter for the message config file
-     *
-     * @return message config
-     */
-    public static YamlConfiguration getConfig() {
-        if (config == null) {
-            reloadConfig();
-        }
-        return config;
-    }
-
-    /**
-     * Saves the message config file
-     */
-    public static void saveConfig() {
-        if (config == null || messageConfigFile == null) {
-            return;
-        }
-        try {
-            getConfig().save(messageConfigFile);
-        } catch (IOException ex) {
-            Volleyball.plugin.getLogger().log(Level.SEVERE, "Could not save config to " + messageConfigFile, ex);
-        }
-    }
-
-    /**
-     * Saves default message config when the server loads
-     */
-    public static void saveDefaultConfig() {
-        if (messageConfigFile == null) {
-            messageConfigFile = new File(Volleyball.plugin.getDataFolder(), "messages.yml");
-        }
-        if (!messageConfigFile.exists()) {
-            Volleyball.plugin.saveResource("messages.yml", false);
-            config = YamlConfiguration.loadConfiguration(messageConfigFile);
-        }
-    }
-
+    
 }
