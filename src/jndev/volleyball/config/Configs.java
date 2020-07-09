b71b7267090dce7d4a784e7bc249e32a13307027
Jeremy Noesen <jeremynoesen@gmail.com>
2020-07-09 17:52:06 -0500

    Renamed ConfigManager to Config

--- src/jndev/volleyball/config/Configs.java
+++ src/jndev/volleyball/config/Configs.java
@@ -8,18 +8,18 @@ public class Configs {
     /**
      * court config instance
      */
-    private static ConfigManager court = new ConfigManager(ConfigType.COURT);
+    private static Config court = new Config(ConfigType.COURT);
     
     /**
      * message config instance
      */
-    private static ConfigManager message = new ConfigManager(ConfigType.MESSAGE);
+    private static Config message = new Config(ConfigType.MESSAGE);
     
     /**
      * @param type config type
      * @return config manager for the selected type
      */
-    public static ConfigManager getConfig(ConfigType type) {
+    public static Config getConfig(ConfigType type) {
         
         switch (type) {
             case MESSAGE:
